86f086c0d8155c4a50b5d83a4a39b32cff069aea
Faisal Bin Ahmed <faisalbin@outlook.com>
2024-05-09 16:26:25 +0200

    default bg color

--- src/components/ReadingList.tsx
+++ src/components/ReadingList.tsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "preact/hooks";
 
 type ReadingListItem = {
-	title: string;
-	url: string;
+	title?: string;
+	url?: string;
+	hasBeenRead?: boolean;
 };
+
+function getUrlDomain(url?: string) {
+	if (!url) {
+		return "";
+	}
+	const urlObj = new URL(url);
+	return urlObj.hostname;
+}
 
 const ReadingList = () => {
 	const [listItems, setListItems] = useState<ReadingListItem[]>([]);
@@ -19,16 +28,23 @@ const ReadingList = () => {
 	}
 
 	return (
-		<div>
-			<div className="text-xs text-neutral-100 border-b">Reading List</div>
-			<ul>
+		<div className="w-[300px]">
+			<div className="text-xl">Reading List</div>
+			<div className="flex flex-col gap-2 m-2">
 				{listItems.map((item) => (
-					<li>
-						{item.title}
-						{item.url}
-					</li>
+					<div className="flex flex-row bg-black p-2 rounded">
+						<img
+							src={`chrome://favicon/${item.url}`}
+							alt="favicon"
+							className="w-6 h-6 mr-2"
+						/>
+						<div className="flex-1 flex flex-col gap-1">
+							<div>{item.title}</div>
+							<div>{getUrlDomain(item.url)}</div>
+						</div>
+					</div>
 				))}
-			</ul>
+			</div>
 		</div>
 	);
 };
